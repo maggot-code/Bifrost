40dd879292e320b0dcd4d90c299943a785860df4
maggot_code <wowhuanhuan233@163.com>
2021-07-06 15:15:44 +0800

    #

--- src/components/main/src/index.vue
+++ src/components/main/src/index.vue
@@ -2,23 +2,20 @@
  * @Author: maggot-code
  * @Date: 2021-06-23 09:47:07
  * @LastEditors: maggot-code
- * @LastEditTime: 2021-06-23 15:15:18
+ * @LastEditTime: 2021-07-06 15:12:24
  * @Description: file content
 -->
 <template>
-    <div class="b-main" :style="style">
-        <b-form></b-form>
-    </div>
+    <div class="b-main" :style="style"></div>
 </template>
 
 <script lang='ts'>
-import { default as BForm } from "@pkg/component/form";
 import { defineComponent } from "vue";
 
 export default defineComponent({
     name: "b-main",
 
-    components: { BForm },
+    components: {},
 
     props: {
         offsetH: {
