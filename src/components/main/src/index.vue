bf5a59f3faa9f4c347046d5b303418a164cb16cc
maggot_code <wowhuanhuan233@163.com>
2021-07-12 13:36:55 +0800

    #

--- src/components/main/src/index.vue
+++ src/components/main/src/index.vue
@@ -2,25 +2,20 @@
  * @Author: maggot-code
  * @Date: 2021-06-23 09:47:07
  * @LastEditors: maggot-code
- * @LastEditTime: 2021-07-06 15:33:41
+ * @LastEditTime: 2021-07-12 13:15:17
  * @Description: file content
 -->
 <template>
-    <div class="b-main" :style="style">
-        <bi-input></bi-input>
-        <el-input></el-input>
-    </div>
+    <div class="b-main" :style="style">main</div>
 </template>
 
 <script lang='ts'>
 import { defineComponent } from "vue";
-import { default as BiInput } from "@pkg/component/input";
-import { default as UseElInput } from "@pkg/composables/bi-input";
 
 export default defineComponent({
     name: "b-main",
 
-    components: { BiInput },
+    components: {},
 
     props: {
         offsetH: {
@@ -37,10 +32,7 @@ export default defineComponent({
         },
     },
 
-    setup() {
-        const elInputProps = UseElInput({});
-        console.log(elInputProps);
-    },
+    setup() {},
 
     beforeCreate() {},
 
